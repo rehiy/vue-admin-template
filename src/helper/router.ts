b71a8fa1dae8bcf64ca7a983d2acbe9a09df3575
若海 <wang@rehiy.com>
2022-06-01 01:39:17 +0800

    归类错误页面

--- src/helper/router.ts
+++ src/helper/router.ts
@@ -94,20 +94,20 @@ const routes = [
         component: () => import('@/apps/tabs.vue'),
       },
       {
-        path: '/404',
-        name: '404',
-        meta: {
-          title: '找不到页面',
-        },
-        component: () => import('@/apps/404.vue'),
-      },
-      {
         path: '/403',
         name: '403',
         meta: {
           title: '没有权限',
         },
-        component: () => import('@/apps/403.vue'),
+        component: () => import('@/apps/error/403.vue'),
+      },
+      {
+        path: '/404',
+        name: '404',
+        meta: {
+          title: '找不到页面',
+        },
+        component: () => import('@/apps/error/404.vue'),
       },
     ],
   },
