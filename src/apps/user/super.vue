aaf6a3fc0c4d17d19bce5e8510c8c8cba5632d21
若海 <wang@rehiy.com>
2022-06-07 09:23:40 +0800

    修复路由

--- src/apps/user/super.vue
+++ src/apps/user/super.vue
@@ -15,7 +15,7 @@
             <p>只有用 admin 账号登录的才拥有管理员权限，才能进到这个页面，其他账号想进来都会跳到403页面，重新用管理员账号登录才有权限。</p>
             <p>
                 想尝试一下，请
-                <router-link to="/login" class="logout">退出登录</router-link>，随便输入个账号名，再进来试试看。
+                <router-link to="/user/login" class="logout">退出登录</router-link>，随便输入个账号名，再进来试试看。
             </p>
         </div>
     </div>
